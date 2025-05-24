f238fa055c3138277180aa2a7525d77e40f9c90c
i3ima <hueputalo.rubin@gmail.com>
2025-05-24 15:31:10 +0200

    fix: pin mason version

--- init.lua
+++ init.lua
@@ -629,8 +629,8 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = {} },
-      'williamboman/mason-lspconfig.nvim',
+      { 'williamboman/mason.nvim', opts = {}, version = '^1.0.0' },
+      { 'williamboman/mason-lspconfig.nvim', version = '^1.0.0' },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
